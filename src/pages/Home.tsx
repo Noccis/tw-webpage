c86b10311c621616e09ede26a0bf3a1d57621666
Noccis <toniwincent83@gmail.com>
2024-09-01 20:26:36 +0200

    Created a landing-page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import '../styles/home.css'
+import portrait from "../assets/profileColor1.png"
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { FaLinkedin } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaGithub } from "react-icons/fa6";
 
 const Home = () => {
+  const [typedWords] = useTypewriter({
+    words: ['Och jag är en kreativ utvecklare',]
+  })
+
   return (
-    <div>Home</div>
+    <div id="home-container">
+      <div id="home-text">
+        <p className="data-styled-text">Hej jag heter</p>
+        <h1>Toni Wincent</h1>
+        <p className="grey-text">
+          {typedWords}
+        </p>
+        <ul id="contact-info">
+          <li>
+            <p><a href="https://www.linkedin.com/in/toni-lof/" target="_blank"><FaLinkedin size={30} /></a></p>
+          </li>
+          <li>
+            <p><a href="https://github.com/Noccis" target="_blank"><FaGithub size={30} /></a></p>
+          </li>
+          <li>
+            <p><a href="mailto:toniwincent83@gmail.com"><IoIosMail size={30} /></a></p>
+          </li>
+        </ul>
+      </div>
+      <img src={portrait} id="portrait" />
+    </div>
   )
 }
 
