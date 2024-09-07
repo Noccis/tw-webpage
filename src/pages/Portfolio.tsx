c548428bb6fe923b31b2ff99e9ab7e99e0b08c96
Noccis <toniwincent83@gmail.com>
2024-09-07 16:41:04 +0200

    Changed CSS, updated local json file and designed the project items

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -6,16 +6,25 @@ const Portfolio = () => {
   let listOfProjects = null;
 
   if (data != null) {
-    listOfProjects = data.map(project => (
-      <div key={project.title} id='project-container'>
-        <h1>{project.title}</h1>
-        <p>{project.description}</p>
-        <img className='project-image' src={project.image_url}/>
+    listOfProjects = data.map((project, index) => (
+      <div key={index} id='project-item'>
+        <div id='project-text'>
+          <h2>{project.title}</h2>
+          <p>{project.description}</p>
+          <div id='tag-container'>
+            {
+              project.tags.map((tag, index) =>
+              <p key={index}>{tag}</p>)
+            }
+          </div>
+          
+        </div>       
+        <img className='project-image' src={project.image_url} />
       </div>
     )
     )
-
   }
+
   return (
     <div id='portfolio-container'>
       {listOfProjects}
